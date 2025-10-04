c43456dd5f21df1e89e3ed3b0f06c698c6eca438
Андрей Шалалуев <unlean@MacBook-Pro-Andrej.local>
2025-10-04 23:23:57 +0300

    Backend: Implemented JWT refresh, fixed event organizer, and set permissions

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# backend/api/views.py
 
-# Create your views here.
+from rest_framework import viewsets # <-- ДОБАВЬТЕ ЭТО
+from .models import Event          # <-- ДОБАВЬТЕ ЭТО (импортируем модель Event из текущего приложения)
+from .serializers import EventSerializer # <-- ДОБАВЬТЕ ЭТО (импортируем сериализатор EventSerializer)
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated] # Теперь для всех операций с событиями нужна авторизация
+
+    def perform_create(self, serializer):
+        # Автоматически устанавливаем текущего пользователя как организатора
+        serializer.save(organizer=self.request.user)
